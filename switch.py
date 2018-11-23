868713d7dfb251777cb33340adc0056ba89d1411
Raul Novelo Cruz <rnovelo.cruz98@gmail.com>
2018-11-23 14:12:23 -0600

    Switch/Case

---
+++ switch.py
@@ -0,0 +1,9 @@
+class switch(object):
+    value = None
+
+    def __new__(class_, value):
+        class_.value = value
+        return True
+
+def case(*args):
+    return any((arg == switch.value for arg in args))
